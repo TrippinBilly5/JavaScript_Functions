8f53865aac891ccc6091adb76945d07f6932f5eb
mark aldridge <mark.aldridge1@protonmail.com>
2024-10-18 14:28:55 -0700

    JavaScript Functions exercises 4 - 5

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 function printOdds(count) {
-    if(count < 0) {
+    if (count < 0) {
         count = count * -1;
     }
     for (let i = 0; i <= count; i++) {
-        if(i % 2 > 0){
+        if (i % 2 > 0) {
             console.log(i);
         }
     }
@@ -16,11 +16,11 @@ printOdds(-30);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-function checkAge(userName = "no Name", age = 15){
+function checkAge(userName = "no Name", age = 15) {
     let aboveSixteen = "Congrats " + userName + ", you can drive!";
     let belowSixteen = "Sorry " + userName + ", but you need to wait until you're 16.";
     let res;
-    if (age >= 16){
+    if (age >= 16) {
         res = aboveSixteen;
     } else {
         res = belowSixteen;
@@ -31,7 +31,7 @@ checkAge("John", 16);
 checkAge("Sally", 10);
 checkAge("Fred", 32);
 checkAge("Joe");
-    
+
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 function findQuadrant(x, y) {
@@ -57,6 +57,39 @@ findQuadrant(45, -2);
 
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
+function checkTriangle(side1, side2, side3) {
+    if (side1 + side2 < side3) {
+        console.log("Invalid triangle.  The sum of side1 and side2 are less than side3. --> " + side1 + ", " + side2 + ", " + side3);
+    } else if (side1 == side2 && side2 == side3) {
+        console.log("Equilateral triangle. --> " + side1 + ", " + side2 + ", " + side3);
+    } else if (side1 == side2) {
+        console.log("Isosceles triangle. --> " + side1 + ", " + side2 + ", " + side3);
+    } else {
+        console.log("Scalene triangle. --> " + side1 + ", " + side2 + ", " + side3);
+    }
+}
+checkTriangle(1, 2, 5);
+checkTriangle(7, 7, 5);
+checkTriangle(5, 5, 5);
+checkTriangle(8, 2, 5);
 
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
+function planDataStatus(planLimit, day, usage) {
+    let daysRemaining = 30 - day;
+    let dataRemaining = planLimit - usage;
+    let AvgCurrUsage = usage / day;
+    let avgMonthlyAllotment = planLimit / 30;
+    console.log(day + " days used, " + daysRemaining + " days remaining");
+    console.log("Average daily use: " + AvgCurrUsage + " GB/day");
+    if (avgMonthlyAllotment < AvgCurrUsage) {
+        console.log("You are EXCEEDING your average daily use (" + avgMonthlyAllotment + " GB/day allotted),");
+        console.log("Continuing this high usage, you'll exceed your data plan by " + ((AvgCurrUsage * 30) - planLimit) + " GB.");
+        console.log("To stay below your data plan, use no more than " + (dataRemaining / daysRemaining) + " GB/day.");
+    } else {
+        console.log("No issues.  Your average daily use is less than the monthly daily average (" + avgMonthlyAllotment + " GB/day)");
+    }
+    console.log("******************");
+}
+planDataStatus(200, 3, 24);
+planDataStatus(100, 5, 15);
